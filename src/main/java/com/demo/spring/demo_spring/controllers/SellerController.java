3050e64803409203829916455819d86dacb38202
J Bruno <jbrunorodrigues13@gmail.com>
2026-04-10 17:33:58 -0300

    feat: implement SellerService

--- src/main/java/com/demo/spring/demo_spring/controllers/SellerController.java
+++ src/main/java/com/demo/spring/demo_spring/controllers/SellerController.java
@@ -1,5 +1,8 @@
 package com.demo.spring.demo_spring.controllers;
 
+import com.demo.spring.demo_spring.services.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/demo/spring/demo_spring/services/SellerService.java
+++ src/main/java/com/demo/spring/demo_spring/services/SellerService.java
@@ -1,7 +1,37 @@
 package com.demo.spring.demo_spring.services;
 
+import com.demo.spring.demo_spring.entities.Seller;
+import com.demo.spring.demo_spring.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SellerService {
+
+    @Autowired
+    private SellerRepository repository;
+
+    public List<Seller> findAll() {
+        return repository.findAll();
+    }
+
+    public Seller findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Seller not found"));
+    }
+
+    public Seller insert(Seller sel) {
+        return repository.save(sel);
+    }
+
+    public Seller update(Long id, Seller sel) {
+        Seller obj = repository.findById(id).orElseThrow(() -> new RuntimeException("Seller not found"));
+        obj.setName(sel.getName());
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
